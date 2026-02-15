eb8ac7ef1c59335581f7a1e9acd84d6a3b900fb2
Richard Yue <44269389+rsyue@users.noreply.github.com>
2026-02-14 21:25:53 -0800

    Organized into src dir for future PyPi

--- launch.sh
+++ launch.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
-# source ~/sft/.venv/bin/activate
+# Example training script to run in background
+# source .venv/bin/activate
 
 nohup python deberta.py -bs=8 -ld=0.3 -wd=0.1 -lr=2e-5 -g=0.9 -ep=2 --fp16 > /tmp/stdout.tmp 2> /tmp/stderr.tmp &
 PID=$!
